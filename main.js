898149a609a97358ab4275faec730aac42d8de21
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-06-09 17:24:06 +0300

    Complete project until assignment 3

--- main.js
+++ main.js
@@ -12,23 +12,25 @@ function Node(data) {
 export function Tree(array) {
 
     function buildTree(array) {
-        const sortedArr = [...new Set(array)].sort((a, b) => a - b);
+        const sortedArr = [...new Set(array)].sort((a, b) => a - b); // sort and 
+        // remove duplicates from array
 
-        function build(start, end) {
+        function build(start, end) { // helper function to build root and subtrees
             if (start > end) { //end of recursion
                 return null;
             }
 
             let mid = Math.floor((start + end) / 2);
-            const root = Node(sortedArr[mid]); // setting root of 0-level
+            const root = Node(sortedArr[mid]); //  root of current subtree
 
-            root.left = build(start, mid - 1); // left part
-            root.right = build(mid + 1, end); // right part
+            root.left = build(start, mid - 1); // left subtree
+            root.right = build(mid + 1, end); // right subtree
 
             return root;
         }
 
-        return build(0, sortedArr.length - 1); // building binary tree for entire array
+        return build(0, sortedArr.length - 1); // building full balanced binary tree 
+        // for entire array
     }
 
     return buildTree(array);
--- test.js
+++ test.js
@@ -1,3 +1,16 @@
 import { Tree } from "./main.js";
 
-const test = new Tree(array);
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node === null || node === undefined) {
+        return;
+    }
+
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+}
+
+
+const test = new Tree([2, 4, 5, 7, 1, 3, 6]);
+
+prettyPrint(test);
